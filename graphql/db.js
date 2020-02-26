fc84a79f8102b3f9d3bcd7baea881b5507909b72
Unknown <prpr48@naver.com>
2020-02-27 00:28:36 +0900

    fetch from rest api

--- graphql/db.js
+++ graphql/db.js
@@ -1,58 +1,8 @@
-let movies = [
-  {
-    id: 0,
-    name: 'jh3618.kim',
-    score: 30
-  },
-  {
-    id: 1,
-    name: 'dadfd',
-    score: 30
-  },
-  {
-    id: 2,
-    name: 'fdgeim',
-    score: 30
-  },
-  {
-    id: 3,
-    name: 'geim',
-    score: 30
-  },
-  {
-    id: 4,
-    name: 'jh3qwqwm',
-    score: 30
-  },
-  {
-    id: 5,
-    name: 'jsdkim',
-    score: 30
-  }
-];
-export const getMovies = () => movies;
+import fetch from 'node-fetch';
 
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
-};
+const API_URL = 'https://yts.am/api/v2/list_movies.json';
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
+export const getMovies = (limit, rating) =>
+  fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
